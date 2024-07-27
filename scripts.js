c1e0fbe96e8fb751cc65b98f113c0b14a3bfa3a9
AyushMali-17 <ayushmali1708@gmail.com>
2024-07-27 23:33:58 +0000

    started with RecipeBOOK

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// taking help from ChatGpt while coding , and understanding the concepts and code via YT
+
+// scripts.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Document loaded');
+});
